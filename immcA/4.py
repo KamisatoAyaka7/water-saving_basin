6fcc99307b76d0dcaa6d269e3a24ff3a6f34f798
沈仲仪 <142191392+KamisatoAyaka7@users.noreply.github.com>
2024-11-25 11:24:40 +0800

    Add files via upload

--- immcA/4.py
+++ immcA/4.py
@@ -1,13 +1,13 @@
-def cGmax(Cwater,H,Sc,Cs,Tlim,Tms_py,C_py):
+def cGmax(xy,Cwater,H,Sc,CwbCws,CwrCwp):
 	L=[];rst=[0,0,0];G=0
 	for i in range(1000,2500):
 		L.append(i/1000)
 	for n in range(0,10):
 		for l in L:
-			G=Cwater*Sc*l*n/(l*n+l+1)*Tlim*Tms_py-n*Cs*l-C_py*Tlim #bugs here
+			G=xy*Cwater*H*Sc*n/(n+1+1/l)-n*l*(CwbCws+xy*CwrCwp) #bugs
 			if(G-rst[0]>0):
 				rst[0]=G;rst[1]=l;rst[2]=n
 		print("@: ",n)
 	print("when Gmax=",rst[0],"λ=",rst[1],"n=",rst[2])
 
-cGmax(0.57,23.65,23485,333333333,50,39*356,10000000)
+cGmax(50*30*365,0.57,23.65,23485,333333333,10000)
